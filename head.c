8a10d85842e71e734e55dc83fb800935abe80c61
koolii <kuriyama@createentrance.com>
2017-11-08 18:23:58 +0100

    implements option

--- head.c
+++ head.c
@@ -1,35 +1,58 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// getopt_long()のプロトタイプ宣言を取得するための定義
+#define _GNU_SOURCE
+#include <getopt.h>
+
 static void do_head(FILE *f, long nlines);
+
+#define DEFAULT_N_LINES 10
+
+// 下記を同時に実行
+// * longopts変数を宣言
+// * 配列を定義(※最後の0の配列はgetopt_long()側にオプションが最後であることを伝えたいから)
+static struct option longopts[] = {
+  {"lines", required_argument, NULL, 'n'},
+  {"help", no_argument, NULL, 'h'}, // 「--help == -h」と定義
+  {0, 0, 0, 0},
+};
 
 int
 main(int argc, char *argv[])
 {
-  long nlines;
+  int opt;
+  long nlines = DEFAULT_N_LINES;
 
-  if (argc < 2) {
-    fprintf(stderr, "Usage: %s n\n", argv[0]);
-    exit(1);
+  while ((opt = getopt_long(argc, argv, "n:", longopts, NULL)) != -1) {
+    switch (opt) {
+    case 'n':
+      // optarg内にオプションに渡したパラメータが格納されている
+      nlines = atol(optarg);
+      break;
+    case 'h':
+      fprintf(stdout, "Usage: %s [-n LINES] [FILE ...]\n", argv[0]);
+      exit(0);
+    case '?':
+      fprintf(stderr, "Usage: %s [-n LINES] [FILE ...]\n", argv[0]);
+      exit(1);
+    }
   }
 
-  // atolは文字列をlong型に変換
-  // atoiは文字列をint型に変換
-  // 一番最初の引数は何行表示させるのか設定
-  nlines = atol(argv[1]);
-  if (argc == 2) {
+  // optindはgetopt() or getopt_long()をループを抜けた時点での
+  // オプションではない最初の引数のインデックスになる
+  if (optind == argc) {
     do_head(stdin, nlines);
   } else {
     int i;
 
-    // 引数2つ目以降は出力するパスを指定する(可変長引数)
-    for (i = 2; i < argc; i++) {
+    for (i = optind; i < argc; i++) {
       FILE *f;
 
       f = fopen(argv[i], "r");
-      if (f == NULL) {
+      if (!f) {
         perror(argv[i]);
-	exit(1);
+        exit(1);
       }
 
       do_head(f, nlines);
@@ -58,7 +81,6 @@ do_head(FILE *f, long nlines)
       nlines--;
       if (nlines == 0) return;
     }
-
   }
 
 }
