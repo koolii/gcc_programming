d90749b815e57f20cd29201f58a1871e7b9751e0
koolii <koolii0909@gmail.com>
2017-11-28 23:09:02 +0100

    add http server's comments

--- src/http_server/main.c
+++ src/http_server/main.c
@@ -1,3 +1,22 @@
+// RESULT
+// ~/w/linux_system ❯❯❯ ./binary/httpd .
+// GET /src/hello.c HTTP/1.0
+//
+// HTTP/1.0 200 OK
+// Date: Mon, 27 Nov 2017 22:59:37 GMT
+// Server: LittleHTTP/1.0
+// Connection: close
+// Content-Length: 97
+// Content-Type: text/plain
+//
+// #include <stdio.h>
+//
+// int
+// main(int argc, char *argv[])
+// {
+//   printf("Hello, world\n");
+//   return 0;
+// }
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -18,6 +37,7 @@
 #define BLOCK_BUF_SIZE 1024
 #define LINE_BUF_SIZE 4096
 #define MAX_REQUEST_BODY_LENGTH (1024 * 1024)
+#define TIME_BUF_SIZE 64
 
 /****** Function Prototypes **********************************************/
 typedef void (*sighandler_t)(int);
@@ -62,25 +82,6 @@ struct HTTPRequest {
   long length;
 };
 
-// RESULT
-// ~/w/linux_system ❯❯❯ ./binary/httpd .
-// GET /src/hello.c HTTP/1.0
-//
-// HTTP/1.0 200 OK
-// Date: Mon, 27 Nov 2017 22:59:37 GMT
-// Server: LittleHTTP/1.0
-// Connection: close
-// Content-Length: 97
-// Content-Type: text/plain
-//
-// #include <stdio.h>
-//
-// int
-// main(int argc, char *argv[])
-// {
-//   printf("Hello, world\n");
-//   return 0;
-// }
 int
 main(int argc, char *argv[])
 {
@@ -96,6 +97,20 @@ main(int argc, char *argv[])
   service(stdin, stdout, argv[1]);
   exit(0);
 }
+
+// 今この時点でこの処理がこのなかで一番大事
+// やっていることはHTTPリクエストを展開して、reqのレスポンスをoutに書き込む
+// 今回はdocrootを書き込むことになる
+static void
+service(FILE *in, FILE *out, char *docroot)
+{
+  struct HTTPRequest *req;
+
+  req = read_request(in);
+  respond_to(req, out, docroot);
+  free_request(req);
+}
+
 
 /* ここからはHTTPRequestの処理 */
 
@@ -320,6 +335,7 @@ read_request(FILE *in)
   return req;
 }
 
+// 大文字に置換する
 static void
 upcase(char *str)
 {
@@ -328,20 +344,6 @@ upcase(char *str)
   for (p = str; *p; p++) {
     *p = (char)toupper((int)*p);
   }
-}
-
-// 今この時点でこの処理がこのなかで一番大事
-// やっていることはHTTPリクエストを展開して、reqのレスポンスをoutに書き込む
-// 今回はdocrootを書き込むことになる
-static void
-service(FILE *in, FILE *out, char *docroot)
-{
-  struct HTTPRequest *req;
-
-  // TODO
-  req = read_request(in);
-  respond_to(req, out, docroot);
-  free_request(req);
 }
 
 /* ここからはHTTPResponseの処理 */
@@ -382,12 +384,14 @@ build_fspath(char *docroot, char *urlpath)
 {
   char *path;
 
+  // 夫々の + 1は "/" と末尾の "\0" のためにメモリの割当量を増やしている
   path = xmalloc(strlen(docroot) + 1 + strlen(urlpath) + 1);
   sprintf(path, "%s%s", docroot, urlpath);
 
   return path;
 }
 
+// 現在はHEAD、GETしか対応していない
 static void
 respond_to(struct HTTPRequest *req, FILE *out, char *docroot) {
   if (strcmp(req->method, "GET") == 0) do_file_response(req, out, docroot);
@@ -399,118 +403,124 @@ respond_to(struct HTTPRequest *req, FILE *out, char *docroot) {
 static void
 do_file_response(struct HTTPRequest *req, FILE *out, char *docroot)
 {
-    struct FileInfo *info;
+  struct FileInfo *info;
 
-    info = get_fileinfo(docroot, req->path);
-    if (!info->ok) {
-        free_fileinfo(info);
-        not_found(req, out);
-        return;
-    }
-    output_common_header_fields(req, out, "200 OK");
-    fprintf(out, "Content-Length: %ld\r\n", info->size);
-    fprintf(out, "Content-Type: %s\r\n", guess_content_type(info));
-    fprintf(out, "\r\n");
-    if (strcmp(req->method, "HEAD") != 0) {
-        int fd;
-        char buf[BLOCK_BUF_SIZE];
-        ssize_t n;
-
-        fd = open(info->path, O_RDONLY);
-        if (fd < 0)
-            log_exit("failed to open %s: %s", info->path, strerror(errno));
-        for (;;) {
-            n = read(fd, buf, BLOCK_BUF_SIZE);
-            if (n < 0)
-                log_exit("failed to read %s: %s", info->path, strerror(errno));
-            if (n == 0)
-                break;
-            if (fwrite(buf, 1, n, out) < n)
-                log_exit("failed to write to socket");
-        }
-        close(fd);
-    }
-    fflush(out);
+  // docrootとパスからファイル情報を取得する
+  info = get_fileinfo(docroot, req->path);
+  if (!info->ok) {
     free_fileinfo(info);
+    not_found(req, out);
+    return;
+  }
+
+  // ここでヘッダー情報の出力を行う
+  // output_common_header_fieldsはステータスコードを生成する共通処理
+  output_common_header_fields(req, out, "200 OK");
+  fprintf(out, "Content-Length: %ld\r\n", info->size);
+  fprintf(out, "Content-Type: %s\r\n", guess_content_type(info));
+  fprintf(out, "\r\n");
+
+  // HEADじゃなければファイルの中身を出力する（今回の内容だとほぼcatと同一になる
+  // レスポンスボディに相当する
+  if (strcmp(req->method, "HEAD") != 0) {
+    int fd;
+    char buf[BLOCK_BUF_SIZE];
+    ssize_t n;
+
+    // ファイルディスクリプタを取得
+    fd = open(info->path, O_RDONLY);
+    if (fd < 0) log_exit("failed to open %s: %s", info->path, strerror(errno));
+    for (;;) {
+      // fdを読み込む
+      n = read(fd, buf, BLOCK_BUF_SIZE);
+
+      if (n < 0) log_exit("failed to read %s: %s", info->path, strerror(errno));
+      if (n == 0) break;
+      if (fwrite(buf, 1, n, out) < n) log_exit("failed to write to socket");
+    }
+    close(fd);
+  }
+  fflush(out);
+  free_fileinfo(info);
 }
 
 static void
 method_not_allowed(struct HTTPRequest *req, FILE *out)
 {
-    output_common_header_fields(req, out, "405 Method Not Allowed");
-    fprintf(out, "Content-Type: text/html\r\n");
-    fprintf(out, "\r\n");
-    fprintf(out, "<html>\r\n");
-    fprintf(out, "<header>\r\n");
-    fprintf(out, "<title>405 Method Not Allowed</title>\r\n");
-    fprintf(out, "<header>\r\n");
-    fprintf(out, "<body>\r\n");
-    fprintf(out, "<p>The request method %s is not allowed</p>\r\n", req->method);
-    fprintf(out, "</body>\r\n");
-    fprintf(out, "</html>\r\n");
-    fflush(out);
+  output_common_header_fields(req, out, "405 Method Not Allowed");
+  fprintf(out, "Content-Type: text/html\r\n");
+  fprintf(out, "\r\n");
+  fprintf(out, "<html>\r\n");
+  fprintf(out, "<header>\r\n");
+  fprintf(out, "<title>405 Method Not Allowed</title>\r\n");
+  fprintf(out, "<header>\r\n");
+  fprintf(out, "<body>\r\n");
+  fprintf(out, "<p>The request method %s is not allowed</p>\r\n", req->method);
+  fprintf(out, "</body>\r\n");
+  fprintf(out, "</html>\r\n");
+  fflush(out);
 }
 
 static void
 not_implemented(struct HTTPRequest *req, FILE *out)
 {
-    output_common_header_fields(req, out, "501 Not Implemented");
-    fprintf(out, "Content-Type: text/html\r\n");
-    fprintf(out, "\r\n");
-    fprintf(out, "<html>\r\n");
-    fprintf(out, "<header>\r\n");
-    fprintf(out, "<title>501 Not Implemented</title>\r\n");
-    fprintf(out, "<header>\r\n");
-    fprintf(out, "<body>\r\n");
-    fprintf(out, "<p>The request method %s is not implemented</p>\r\n", req->method);
-    fprintf(out, "</body>\r\n");
-    fprintf(out, "</html>\r\n");
-    fflush(out);
+  output_common_header_fields(req, out, "501 Not Implemented");
+  fprintf(out, "Content-Type: text/html\r\n");
+  fprintf(out, "\r\n");
+  fprintf(out, "<html>\r\n");
+  fprintf(out, "<header>\r\n");
+  fprintf(out, "<title>501 Not Implemented</title>\r\n");
+  fprintf(out, "<header>\r\n");
+  fprintf(out, "<body>\r\n");
+  fprintf(out, "<p>The request method %s is not implemented</p>\r\n", req->method);
+  fprintf(out, "</body>\r\n");
+  fprintf(out, "</html>\r\n");
+  fflush(out);
 }
 
 static void
 not_found(struct HTTPRequest *req, FILE *out)
 {
-    output_common_header_fields(req, out, "404 Not Found");
-    fprintf(out, "Content-Type: text/html\r\n");
-    fprintf(out, "\r\n");
-    if (strcmp(req->method, "HEAD") != 0) {
-        fprintf(out, "<html>\r\n");
-        fprintf(out, "<header><title>Not Found</title><header>\r\n");
-        fprintf(out, "<body><p>File not found</p></body>\r\n");
-        fprintf(out, "</html>\r\n");
-    }
-    fflush(out);
+  output_common_header_fields(req, out, "404 Not Found");
+  fprintf(out, "Content-Type: text/html\r\n");
+  fprintf(out, "\r\n");
+  if (strcmp(req->method, "HEAD") != 0) {
+      fprintf(out, "<html>\r\n");
+      fprintf(out, "<header><title>Not Found</title><header>\r\n");
+      fprintf(out, "<body><p>File not found</p></body>\r\n");
+      fprintf(out, "</html>\r\n");
+  }
+  fflush(out);
 }
 
 static void
 free_fileinfo(struct FileInfo *info)
 {
-    free(info->path);
-    free(info);
+  free(info->path);
+  free(info);
 }
 
+// 今回は必ずtext/plainとなる
 static char*
 guess_content_type(struct FileInfo *info)
 {
-    return "text/plain";   /* FIXME */
+  return "text/plain";
 }
-
-#define TIME_BUF_SIZE 64
 
 static void
 output_common_header_fields(struct HTTPRequest *req, FILE *out, char *status)
 {
-    time_t t;
-    struct tm *tm;
-    char buf[TIME_BUF_SIZE];
+  time_t t;
+  struct tm *tm;
+  char buf[TIME_BUF_SIZE];
 
-    t = time(NULL);
-    tm = gmtime(&t);
-    if (!tm) log_exit("gmtime() failed: %s", strerror(errno));
-    strftime(buf, TIME_BUF_SIZE, "%a, %d %b %Y %H:%M:%S GMT", tm);
-    fprintf(out, "HTTP/1.%d %s\r\n", HTTP_MINOR_VERSION, status);
-    fprintf(out, "Date: %s\r\n", buf);
-    fprintf(out, "Server: %s/%s\r\n", SERVER_NAME, SERVER_VERSION);
-    fprintf(out, "Connection: close\r\n");
+  t = time(NULL);
+  tm = gmtime(&t);
+  if (!tm) log_exit("gmtime() failed: %s", strerror(errno));
+
+  strftime(buf, TIME_BUF_SIZE, "%a, %d %b %Y %H:%M:%S GMT", tm);
+  fprintf(out, "HTTP/1.%d %s\r\n", HTTP_MINOR_VERSION, status);
+  fprintf(out, "Date: %s\r\n", buf);
+  fprintf(out, "Server: %s/%s\r\n", SERVER_NAME, SERVER_VERSION);
+  fprintf(out, "Connection: close\r\n");
 }
