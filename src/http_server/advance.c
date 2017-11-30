1ce20c8756edf0dd8a383daac7c76a2751573f55
koolii <koolii0909@gmail.com>
2017-11-30 21:38:36 +0100

    [WIP] add create network connection using socket(),bind(),listen()

--- src/http_server/advance.c
+++ src/http_server/advance.c
@@ -175,3 +175,92 @@ main(int argc, char *argv[])
   server_main(server_fd, docroot);
   exit(0);
 }
+
+/*
+ * サーバ側でのgetaddrinfo()の使い方について
+ * クライアントとしてソケット接続するときは接続する先のホストのアドレスをgetaddrinfo()de
+ * アドレス構造体に直しましたが、サーバ側のときはプロセスが動作しているそのホストの
+ * アドレス構造体をgetaddrinfo()で得る　その場合は、getaddrinfo()の第一引数はNULLにし、
+ * また第三引数のai_flagsにAI_PASSIVEを追加する
+ *
+ */
+static int
+listen_socket(char *port)
+{
+  struct addrinfo hints, *res, *ai;
+  int err;
+
+  memset(&hints, 0, sizeof(struct addrinfo));
+  hints.ai_family = AF_INET;
+  hints.ai_socketype = SOCK_STREAM;
+  // ソケットをサーバ用に使うことを指示している
+  // 上述でも書いてある
+  hints.ai_flags = AI_PASSIVE;
+
+  if ((err = getaddrinfo(NULL, port, &hints, &res)) != 0) log_exit(gai_strerror(err));
+
+  // hitsに当てはまるアドレス構造体のリストが得られる(res)ので
+  // socket(), bind(), listen()してみて、成功した最初のアドレスを使うことにしている
+  // 通常であればローカルホストのIPv4アドレスが一つ帰るだけのワザワザループを回す必要はない
+  for (ai = res; ai; ai = ai->ai_next) {
+    int sock;
+
+    sock = socket(ai->ai_family, ai->ai_socketype, ai->ai_protocol);
+    if (sock < 0) continue;
+    if (bind(sock, ai->ai_addr, ai->ai_addrlen) < 0) {
+      close(sock);
+      continue;
+    }
+    if (listen(sock, MAX_BACKLOG) < 0) {
+      close(sock);
+      continue;
+    }
+
+    freeaddrinfo(res);
+
+
+    freeaddrinfo(res);
+
+    return sock;
+  }
+
+  log_exit("failed to listen socket");
+  return -1; /* NOT REACH */
+}
+
+
+/*
+ * server_fdはlisten_socket()の戻り値である
+ * つまり、この引数server_fdはソケットを表すファイルディスクリプタである
+ * bind(),listen()したソケットに対してaccept()を呼ぶと、クライアントから接続要求が来るのを待ち、
+ * 接続済みの新しいソケットを返します
+ *   * socket(), bind(), listen()で作るソケットにストリームが接続されることはない
+ *   * 通常、socket(),bind(),listen()はプロセスにつき一回呼べば済むのに対してaccept()は何度も呼ぶ
+ *   => ソケットを作るのはsocket(),bind(),listen()で作ったソケットをストリームに紐付けるのにaccept()を
+ *   使うからストリームに接続する度にaccept()は何度も呼ぶことになる
+ */
+static void
+server_main(int server_fd, char *docroot)
+{
+  for (;;) {
+    struct sockaddr_storage addr;
+    socklen_t addrlen = sizeof addr;
+    int sock;
+    int pid;
+
+    sock = accept(server_fd, (struct sockaddr*)&addr, $addrlen);
+    if (sock < 0) log_exit("accept(2) failed: %s", strerror(errno));
+
+    pid = fork();
+    if (pid < 0) exit(3);
+    if (pid == 0) {
+      FILE *inf = fdopen(sock, "r");
+      FILE *out = fdopen(sock, "w");
+
+      service(inf, outf, docroot);
+      exit(0);
+    }
+
+    close(sock);
+  }
+}
